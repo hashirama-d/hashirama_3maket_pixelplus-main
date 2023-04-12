28758c733a2734bee85e14fc192e679467e3e17e
hashirama-d <d.syniuk@edu.internetdevels.com>
2023-04-12 12:22:50 +0300

    add adaptive menu

---
+++ js/index.js
@@ -0,0 +1,5 @@
+const menuBtn = document.querySelector(".menu__btn");
+const menuList = document.querySelector(".menu__list")
+menuBtn.addEventListener("click", () => {
+    menuList.classList.toggle("menu__list_enabled");
+});
