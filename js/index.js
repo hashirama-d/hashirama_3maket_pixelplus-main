51f4d6dc8fa42169071afcbc81dcadd744a2fc50
hashirama-d <d.syniuk@edu.internetdevels.com>
2023-04-20 10:37:30 +0300

    add all scripts into one file

--- js/index.js
+++ js/index.js
@@ -1,6 +1,28 @@
 const menuBtn = document.querySelector(".menu__btn");
 const menuList = document.querySelector(".menu__list")
+
+//  Тригер кнопки для бургер-меню
 menuBtn.addEventListener("click", () => {
     menuList.classList.toggle("menu__list_enabled");
     menuBtn.classList.toggle("menu__btn_enabled");
+});
+
+//  Налаштування слайдера
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        fade: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+    });
+});
+
+//  Зміна розміру контейнера з слайдером при зменшенні розміру екрана на менше ніж 1400 пікселів
+$(window).on('load resize', function() {
+    var screenWidth = $(window).width();
+    var containerWidth = Math.min(screenWidth, 1400); // Limit to 1400px
+    $('.slider__inner').css('max-width', containerWidth + 'px');
 });
